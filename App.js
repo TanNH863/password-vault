64b4f7f31e79d5cb167bf625eebb48d2158b4755
EdgynCode <nguyenhuutan080603@gmail.com>
2024-08-13 00:59:26 +0700

    add tab navigator

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ContextProviders } from './components/ContextProviders';
 import LanguageScreen from './screens/LanguageScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
@@ -9,8 +10,39 @@ import PINCodeSetup from './screens/PINCodeSetup';
 import FingerprintSetup from './screens/FingerprintSetup';
 import AddPasswordScreen from './screens/main/AddPasswordScreen';
 import MainScreen from './screens/main/MainScreen';
+import SettingScreen from './screens/main/SettingScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function AppMainScreen() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home-outline';
+          } else if (route.name === 'Settings') {
+            iconName = 'settings-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#0377BC',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          display: 'flex',
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={MainScreen} />
+      <Tab.Screen name="Settings" component={SettingScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -22,7 +54,7 @@ export default function App() {
           <Stack.Screen name="SignUp" component={SignInScreen} options={{ headerShown: false }} />
           <Stack.Screen name="PINCodeSetup" component={PINCodeSetup} />
           <Stack.Screen name="FingerprintSetup" component={FingerprintSetup} />
-          <Stack.Screen name="MainScreen" component={MainScreen} />
+          <Stack.Screen name="MainScreen" component={AppMainScreen} options={{ headerShown: false }}/>
           <Stack.Screen name="AddPasswordScreen" component={AddPasswordScreen} />
         </Stack.Navigator>
       </NavigationContainer>
